dbc546f8056ed412228334aaac5651b2b2df1bdf
DNS <dnsbrawler222@gmail.com>
2026-03-14 20:25:26 +0200

    Initial config and weights.

--- Lib.Models.NityNN/Configuration/TinyNNConfig.cs
+++ Lib.Models.NityNN/Configuration/TinyNNConfig.cs
@@ -1,10 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace Lib.Models.TinyNN.Configuration;
 
-namespace Lib.Models.TinyNN.Configuration
+public class TinyNNConfig 
 {
-    internal class TinyNNConfig
+    public int VocabSize { get; set; } 
+    public int EmbeddingSize { get; } 
+    public int ContextSize { get; } 
+
+    public TinyNNConfig(int vocabSize, int embeddingSize = 32, int contextSize = 8)
     {
+        VocabSize = vocabSize;
+        EmbeddingSize = embeddingSize;
+        ContextSize = contextSize;
     }
 }
